0d87aee764a0d7711a4ccfcabe7ec25f6fc23744
Olivier Paroz <github@oparoz.com>
2015-06-25 21:25:08 +0200

    Adding the class is enough

--- js/gallery.js
+++ js/gallery.js
@@ -200,7 +200,6 @@ Gallery.switchToFilesView = function (event) {
 
 	var button = $('#filelist-button');
 	button.addClass('loading');
-	button.find('img').attr('src', OC.imagePath('core', 'loading-small.gif'));
 	OC.redirect(OC.generateUrl(subUrl, params));
 };
 
