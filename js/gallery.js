3671044696a48819fd81d82d7a62883ae2b25cbd
viraj96 <parimiviraj@gmail.com>
2016-03-17 01:54:48 +0530

    share dialog fixation

--- js/gallery.js
+++ js/gallery.js
@@ -360,10 +360,10 @@
 				}
 			};
 			Gallery.activeSlideShow.show(start);
-			if(Gallery.Share){
+			if(!_.isUndefined(Gallery.Share)){
 				Gallery.Share.hideDropDown();
-				$('.album-info-container').slideUp();
 			}
+			$('.album-info-container').slideUp();
 			// Resets the last focused element
 			document.activeElement.blur();
 		},
