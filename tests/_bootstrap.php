548a1fdca8dd1ca72ca1e79419eaedd29a4ba030
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2017-05-15 10:01:09 +0200

    Reduce build matrix (#718)
    * Reduce build matrix
    
    * Fix bootstrap
    
    * Update compose.json -> we need a higher phpunit version

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -17,6 +17,8 @@ define('PHPUNIT_RUN', 1);
 // Add core
 require_once __DIR__ . '/../../../lib/base.php';
 
+\OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
+
 // Add core tests to the list of valid paths
 OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
